02f5320642d1420a6efb903b4c03b2b25cad506f
Luke Philips and Marty Kovach <marty@>
2016-12-13 09:43:43 -0800

    titlecase callback method for Survey

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,4 +1,13 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
   validates(:title, {:presence => true})
+  before_save(:to_titlecase)
+
+
+
+private
+
+  define_method(:to_titlecase) do
+    self.title =(title().titlecase())
+  end
 end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -2,18 +2,22 @@ require('spec_helper')
 
 describe(Survey) do
   before() do
-    @new_survey = Survey.create({:title => "Shopper Survey"})
+    @new_survey = Survey.create({:title => "shopper survey"})
   end
 
   describe('#title') do
     it('will return the title of the survey') do
       new_survey = Survey.create({:title => "Shopper survey"})
-      expect(new_survey.title()).to(eq("Shopper survey"))
+      expect(new_survey.title()).to(eq("Shopper Survey"))
     end
 
     it("ensure the user gives the survey a title") do
       test_survey = Survey.create({:title => " "})
       expect(test_survey.save()).to(eq(false))
+    end
+
+    it("convert the title of the survey to titlecase") do
+      expect(@new_survey.title()).to(eq("Shopper Survey"))
     end
   end
 end
